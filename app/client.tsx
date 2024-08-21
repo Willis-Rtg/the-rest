8b27dc088f0d3ee737533494bc9079b6ad74844e
Willis Kim <kshwan.1023@gmail.com>
2024-08-21 17:29:36 +0900

    swiper

--- app/client.tsx
+++ app/client.tsx
@@ -11,13 +11,11 @@ export default function HomeClient() {
   const slideCount = useRef(0);
   const ref = useRef<HTMLDivElement>(null);
   function onDragStart(event: React.DragEvent) {
-    // event.preventDefault();
-    setDragStart({ x: event.clientX });
+    setDragStart({ x: event.screenX });
   }
 
   function onDrag(event: React.DragEvent) {
-    console.log(event);
-    const moveX = dragStart.x - event.clientX;
+    const moveX = dragStart.x - event.screenX;
     ref.current!.style.transform = `translateX(-${
       402 * slideCount.current + moveX
     }px)`;
@@ -41,83 +39,95 @@ export default function HomeClient() {
     <div>
       <div className="h-[100vh] flex flex-col items-center pt-16 gap-24">
         <h2 className="text-5xl">Room Previews</h2>
-        <div>
-          <div className="relative h-full px-96 overflow-hidden w-[1330px] flex justify-center">
-            <div
-              className="flex"
-              ref={ref}
-              onDragStart={onDragStart}
-              onDrag={onDrag}
-              onDragEnd={onDragEnd}
-              onDragLeave={(e) => e.preventDefault()}
-              onDragOver={(e) => e.preventDefault()}
-            >
-              <div className="flex flex-col items-center flex-shrink-0">
-                <Image
-                  src="/room_preview.png"
-                  alt="room"
-                  width={500}
-                  height={770}
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  alt="room"
-                  width={500}
-                  height={770}
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  width={500}
-                  height={770}
-                  alt="room"
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  width={370}
-                  height={600}
-                  alt="room"
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  width={370}
-                  height={600}
-                  alt="room"
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  width={370}
-                  height={600}
-                  alt="room"
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
-              <div className="flex flex-col items-center flex-shrink-0 ">
-                <Image
-                  src="/room_preview.png"
-                  width={370}
-                  height={600}
-                  alt="room"
-                />
-                <span className="text-xl">A동 101호</span>
-              </div>
+        <Swiper
+          className="px-96 w-[1330px] "
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          scrollbar={true}
+          onSwiper={(swiper) => {
+            console.log((swiper.slides[1].style.height = "700px"));
+          }}
+        >
+          <SwiperSlide>
+            <div className="flex flex-col items-center">
+              <Image
+                src="/room_preview.png"
+                alt="room"
+                width={370}
+                height={600}
+              />
+              <span className="text-xl">A동 101호</span>
             </div>
-          </div>
-        </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center">
+              <Image
+                src="/room_preview.png"
+                alt="room"
+                width={500}
+                height={770}
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center">
+              <Image
+                src="/room_preview.png"
+                width={370}
+                height={600}
+                alt="room"
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="flex flex-col items-center ">
+              <Image
+                src="/room_preview.png"
+                width={370}
+                height={600}
+                alt="room"
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center ">
+              <Image
+                src="/room_preview.png"
+                width={370}
+                height={600}
+                alt="room"
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center ">
+              <Image
+                src="/room_preview.png"
+                width={370}
+                height={600}
+                alt="room"
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="flex flex-col items-center ">
+              <Image
+                src="/room_preview.png"
+                width={370}
+                height={600}
+                alt="room"
+              />
+              <span className="text-xl">A동 101호</span>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
       <div className="h-[100vh] flex flex-col items-center pt-16 gap-24"></div>
     </div>
