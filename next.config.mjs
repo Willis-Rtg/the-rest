a7372ff879bef3dbd33ed0b20fabe9370b3b8ae1
Willis Kim <kshwan.1023@gmail.com>
2025-06-13 14:16:35 +0900

    nextjs bodysizelimit 20mb

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    bodySizeLimit: "20mb",
+  },
   images: {
     remotePatterns: [
       {
